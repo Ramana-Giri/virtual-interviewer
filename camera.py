81013930cb541145b73fb2d84db1e3e6112fbece
Ramana Giri <loamramg@gmail.com>
2025-11-24 18:55:01 +0530

    initial commit

---
+++ camera.py
@@ -0,0 +1,25 @@
+import cv2
+
+
+class VideoCamera(object):
+    def __init__(self):
+        # Open the default camera (0)
+        self.video = cv2.VideoCapture(0)
+
+    def __del__(self):
+        self.video.release()
+
+    def get_frame(self):
+        success, image = self.video.read()
+        if not success:
+            return None
+
+        # --- PLACEHOLDER FOR EMOTION DETECTION ---
+        # Later, we will send 'image' to our AI model here.
+        # For now, let's just draw a rectangle to prove OpenCV is working.
+        cv2.rectangle(image, (100, 100), (400, 400), (0, 255, 0), 2)
+        # -----------------------------------------
+
+        # Encode the frame to JPEG format for the browser
+        ret, jpeg = cv2.imencode('.jpg', image)
+        return jpeg.tobytes()
